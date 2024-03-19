e19374822e9f0ace5394ab7bba4dbc3860188dda
sarvagya.prateek <sarvagya.prateek@jeevansathi.com>
2024-03-19 13:37:05 +0530

    Changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  swcMinify: true,
+  swcLoaderOptions: {
+    jsc: {
+      externalHelpers: true,
+      target: "es2020",
+    },
+  },
+  styledComponents: {
+    swcOptions: {
+      jsc: {
+        externalHelpers: true,
+        target: "es2020",
+      },
+    },
+  },
+};
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,132 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import * as tf from "@tensorflow/tfjs";
+
+import React, { useEffect, useRef, useState } from "react";
+
+function Home() {
+  const [model, setModel] = useState(null);
+
+  useEffect(() => {
+    async function loadModel() {
+      const loadedModel = await mobilenet.load();
+      setModel(loadedModel);
+      console.log("Model loaded");
+    }
+    loadModel();
+  }, []);
+
+  async function compareImages(event) {
+    const imageInput = event.target.files;
+    const uploadedImagesDiv = document.getElementById("uploadedImages");
+    const similarImagesDiv = document.getElementById("similarImages");
+
+    if (imageInput.length < 2) {
+      alert("Please select at least two images.");
+      return;
+    }
+
+    uploadedImagesDiv.innerHTML = "";
+    similarImagesDiv.innerHTML = "";
+
+    const promises = Array.from(imageInput).map((file) =>
+      loadImage(URL.createObjectURL(file)),
+    );
+    const imgs = await Promise.all(promises);
+
+    const features = await Promise.all(imgs.map(extractFeatures));
+
+    const similarityScores = [];
+    for (let i = 0; i < features.length; i++) {
+      for (let j = i + 1; j < features.length; j++) {
+        const similarityScore = calculateSimilarity(features[i], features[j]);
+        similarityScores.push({
+          imageIndex1: i,
+          imageIndex2: j,
+          similarityScore,
+        });
+      }
+    }
+
+    const uniqueSimilarImages = new Set();
+    for (const score of similarityScores) {
+      if (score.similarityScore >= -1 && score.similarityScore < -0.9) {
+        uniqueSimilarImages.add(score.imageIndex1);
+        uniqueSimilarImages.add(score.imageIndex2);
+      }
+    }
+
+    for (const file of imageInput) {
+      const image = document.createElement("img");
+      image.src = URL.createObjectURL(file);
+      image.style.width = "250px";
+      image.style.height = "250px";
+      image.style.marginRight = "10px";
+
+      uploadedImagesDiv.appendChild(image);
+    }
+
+    for (const index of uniqueSimilarImages) {
+      const image = document.createElement("img");
+      image.src = URL.createObjectURL(imageInput[index]);
+      image.style.width = "250px"; // Set width to 40px
+      image.style.height = "250px";
+      image.style.marginRight = "10px";
+      similarImagesDiv.appendChild(image);
+    }
+  }
+
+  async function loadImage(url) {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.crossOrigin = "anonymous";
+      img.onload = () =>
+        resolve(
+          tf.browser
+            .fromPixels(img)
+            .resizeNearestNeighbor([224, 224])
+            .toFloat()
+            .expandDims(),
+        );
+      img.onerror = (err) => reject(err);
+      img.src = url;
+    });
+  }
+
+  async function extractFeatures(image) {
+    const features = model.infer(image, "conv_preds");
+    return features;
+  }
+
+  function calculateSimilarity(featureVector1, featureVector2) {
+    const cosineSimilarity = tf.metrics.cosineProximity(
+      featureVector1,
+      featureVector2,
+    );
+    return cosineSimilarity.dataSync()[0];
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-8">
+      <h1>Image Similarity Detection</h1>
+      <input type="file" id="imageInput" multiple onChange={compareImages} />
+      <h2>All Uploaded Images</h2>
+      <div id="uploadedImages" className="flex shrink-0"></div>
+      <h2>Similar Images</h2>
+      <div id="similarImages" className="flex shrink-0"></div>
+      {/* <h2>Images with Multiple Faces</h2> */}
+      {/* <div className="flex">
+        {imagesWithMultipleFaces.map((image, index) => (
+          <img
+            key={index}
+            src={URL.createObjectURL(image)}
+            style={{ width: "250px", height: "250px", marginRight: "10px" }}
+          />
+        ))}
+      </div> */}
     </main>
   );
 }
+
+export default Home;
